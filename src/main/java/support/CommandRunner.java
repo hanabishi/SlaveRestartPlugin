5b6478a1d4c928ebe6ce79c42c93671b393573e5
marcusja <marcus.jacobsson@schneider-electric.com>
2016-12-27 07:41:57 +0100

    Cleaned up commmand runner

--- src/main/java/support/CommandRunner.java
+++ src/main/java/support/CommandRunner.java
@@ -4,8 +4,6 @@ import hudson.Launcher;
 import hudson.Launcher.ProcStarter;
 
 import java.io.IOException;
-import java.io.PipedOutputStream;
-import java.util.LinkedList;
 
 public class CommandRunner {
 
@@ -18,45 +16,18 @@ public class CommandRunner {
         return proc.join();
     }
 
-    public static LinkedList<String> runCommand(String commandLine, Launcher launcher) throws Exception, IOException,
+    public static void runCommand(String commandLine, Launcher launcher) throws Exception, IOException,
             InterruptedException, IllegalThreadStateException {
-        LinkedList<String> output = new LinkedList<String>();
-        PipedOutputStream pos = null;
-        StreamReaderThread srt = null;
         ProcStarter proc = null;
 
-        try {
-            proc = launcher.launch();
-            pos = new PipedOutputStream();
-            srt = new StreamReaderThread(pos);
-            proc.stdout(pos);
-            proc.stderr(pos);
-            String command = "cmd /c \"" + commandLine + "\"";
-            proc.cmdAsSingleString(command);
-            srt.start();
+        proc = launcher.launch();
+        String command = "cmd /c \"" + commandLine + "\"";
+        proc.cmdAsSingleString(command);
 
-            int code = proc.join();
-            try {
-                pos.close();
-                srt.join();
-            } catch (Exception e) {}
+        int code = proc.join();
 
-            output = srt.getOutput();
-
-            if (code != 0) {
-                throw new Exception("Failed to run command");
-            }
-        } finally {
-            try {
-                if (pos != null) {
-                    pos.close();
-                }
-            } catch (IOException e) {}
-
-            if (srt != null) {
-                srt.kill();
-            }
+        if (code != 0) {
+            throw new Exception("Failed to run command");
         }
-        return output;
     }
 }
